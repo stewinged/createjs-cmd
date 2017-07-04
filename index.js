665f646eddca1d2600e96d46f17f58a3e415c431
yedaodao <404069912@qq.com>
2017-07-04 17:20:44 +0800

    fix dist bug

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 require(
-    'imports-loader?this=>window!\
-    ./lib/easeljs-0.8.2.combined'
+    'imports-loader?this=>window!' +
+    './lib/easeljs-0.8.2.combined'
 );
 require(
-    'imports-loader?this=>window!\
-    ./lib/tweenjs-0.6.2.combined'
+    'imports-loader?this=>window!' +
+    './lib/tweenjs-0.6.2.combined'
 );
 require(
-    'imports-loader?this=>window!\
-    ./lib/preloadjs-0.6.2.combined'
+    'imports-loader?this=>window!' +
+    './lib/preloadjs-0.6.2.combined'
 );
 module.exports = require(
-    'imports-loader?this=>window!\
-    ./lib/soundjs-0.6.2.combined'
+    'imports-loader?this=>window!' +
+    'exports-loader?window.createjs!' +
+    './lib/soundjs-0.6.2.combined'
 );
 
 
